2bd07caea68429601dc03565d64b3c4ceea62d3c
MyManny <gmail.com>
2023-07-20 20:07:27 -0700

    first

--- public/app.js
+++ public/app.js
@@ -1,13 +1,23 @@
 //Note: You do NOT need to create an external JS file for this project despite any instructions you may see in Canvas. This file, app.js, is the only external JS file you need.
 
+var map = L.map('map').setView([51.505, -0.09], 13);
 
+// Create map                                                       
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
 
-// Create map
 
 
 
 
 // Add openstreetmap tiles
+
+L.tileLayer('https://.tile.openstreetmap.org///.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19,
+}).addTo(myMap)
 
 
 
@@ -15,7 +25,8 @@
 
 
 // Create and add a geolocation marker
-
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 
